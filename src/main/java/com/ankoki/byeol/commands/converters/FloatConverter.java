c28ef66f4821e75f6150666545ed721dea6116c5
Ankoki-Dev <ankoki.mc@gmail.com>
2022-09-23 04:22:35 +0100

    Allow shadowJar in jitpack.

--- src/main/java/com/ankoki/byeol/commands/converters/FloatConverter.java
+++ src/main/java/com/ankoki/byeol/commands/converters/FloatConverter.java
@@ -3,6 +3,7 @@ package com.ankoki.byeol.commands.converters;
 import com.ankoki.byeol.commands.ArgumentConverter;
 
 public class FloatConverter extends ArgumentConverter<Float> {
+
 	@Override
 	public Float convert(String argument) {
 		try {
